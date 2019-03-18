ff8a734235e8f31aa1c279d9c2908ac20d875cc4
Rocky Singh <rockygsingh@icloud.com>
2019-03-17 23:52:56 -0400

    wrote comments throughout the file

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 
+// initiain express
 const app = express();
 
-// ejs
+// setting up ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 
-// Routes
+// rendering all the Routes
 app.use('/', require('./routes/index'));
 app.use('/user', require('./routes/user'));
 
