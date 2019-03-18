16e5ca99f45892df38ac0bc38510e25227070af6
Rocky Singh <rockygsingh@icloud.com>
2019-03-17 20:00:49 -0400

    reconfiged all the files

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const expressLayouts = require(express-ejs-layouts);
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// ejs
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+
+// Routes
 app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use('/user', require('./routes/user'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on port {PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
