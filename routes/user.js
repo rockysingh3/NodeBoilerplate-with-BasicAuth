c5e2688d97f8cd075ee87199fb2a59ebe9762cd2
Rocky Singh <rockygsingh@icloud.com>
2019-03-18 22:26:12 -0400

    changes

--- routes/user.js
+++ routes/user.js
@@ -90,4 +90,10 @@ router.post('/login', (req, res, next) => {
     })(req, res, next);
 });
 
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/user/login');
+})
+
 module.exports = router;
